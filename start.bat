b30106d96570268bee5ade7739d5d4ea4ba01a62
summer <summer@gnu.ac.kr>
2023-06-02 19:06:38 +0900

    '_'

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ call D:\tubetoppt\venv\Scripts\activate.bat
 echo "retrive"
 D:\tubetoppt\venv\Scripts\python D:\LRFlashCardWord\LRFlashCardOrder.py
 echo "add"
- git add .
+ git add D:\LRFlashCardWord\.
 echo "commit" 
  git commit -am '_'
 echo "push" 
