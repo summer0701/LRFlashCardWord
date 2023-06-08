d6fd02f2a64ce4cd859bc6642c13c2e0678ac499
summer <summer@gnu.ac.kr>
2023-06-08 16:34:23 +0900

    '_'

--- start.bat
+++ start.bat
@@ -1,12 +1,7 @@
 echo pull
 git pull
-echo activate
-call D:\tubetoppt\venv\Scripts\activate.bat
-echo retrive
-D:\tubetoppt\venv\Scripts\python D:\LRFlashCardWord\LRFlashCardOrder.py
 echo add
 git add D:\LRFlashCardWord\.
-
 echo "commit" 
  git commit -am '_'
 echo "push" 
