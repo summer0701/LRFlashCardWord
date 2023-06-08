9e9e42992ee5378d3871319724b9f5bf391b5ffe
summer <summer@gnu.ac.kr>
2023-06-08 09:56:31 +0900

    08

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
+echo pull
+git pull
 echo activate
 call D:\tubetoppt\venv\Scripts\activate.bat
 echo retrive
 D:\tubetoppt\venv\Scripts\python D:\LRFlashCardWord\LRFlashCardOrder.py
-echo pull
-git pull
 echo add
 git add D:\LRFlashCardWord\.
 
